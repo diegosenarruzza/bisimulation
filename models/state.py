45dd43b8d9b897fc7a33b475ef68fa3afc50012b
dsenarruzza <dsenarruzza@keepcon.com>
2023-02-24 01:16:37 -0300

    Remove deprecated methods on afsm components

--- models/state.py
+++ models/state.py
@@ -1,6 +1,3 @@
-from libs.tools import clean_knowledge_for
-
-
 class State:
 
     def __init__(self, graph, id):
@@ -15,28 +12,3 @@ class State:
 
     def get_transitions_with(self, label):
         return self.graph.transitions_with_label_of(self.id, label)
-
-    def is_able_to_simulate_falling_into(self, simulation_state, knowledge, relation):
-        is_able = True
-        transitions = self.get_transitions()
-        i = 0
-
-        # Si corta porque no cumple is_able, entonces es porque existe una accion que "simulation_state" no puede simular, o que puede pero no cae en la relacion
-        # Si corta porque i < len(transitions) entonces recorrio todas las acciones y "simulation_state" siempre pudo simular a e y caer dentro de la relacion
-        while is_able and i < len(transitions):
-            transition = transitions[i]
-            label = transition.label
-
-            # Saco las assertions cuyas variables van a ser sobreescritas por la transicion actual
-            cleaned_knowledge = clean_knowledge_for(knowledge, label) if label.has_variable() else knowledge
-            simulation_transitions = simulation_state.get_transitions_with(label)
-
-            # Necesito verificar si existe algun subconjunto de transiciones desde "simulation_state", que me sirva para simular la transicion de "self"
-            # Si existe, va a ser unico, ya que si existe mas de un subconjunto que hace esto, quiere decir que existen al menos dos transiciones desde "self"
-            # tq. ambos caminos son validos para una traza valida. Esto nos daria un automata no-determinista, y estamos trabajando siempre con deterministas.
-
-            is_able = transition.exists_a_valid_transition_subset_that_simulates(simulation_transitions, cleaned_knowledge, relation)
-
-            i += 1
-
-        return is_able
--- models/transition.py
+++ models/transition.py
@@ -1,7 +1,3 @@
-from libs.tools import powerset
-from z3 import Solver, sat, And, Or, Implies
-
-
 class Transition:
 
     def __init__(self, source, target, label, assertion):
@@ -12,52 +8,3 @@ class Transition:
 
     def __repr__(self):
         return f"{self.source} -({self.label}, {self.assertion})-> {self.target}"
-
-    def exists_a_valid_transition_subset_that_simulates(self, simulation_transitions, knowledge, relation):
-        # No tomo el subconjunto vacio porque no seria valido. Si no hay mas conjuntos que el vacio, entonces nunca entra al loop y devuelve False
-        simulation_transitions_subsets = list(powerset(simulation_transitions))
-        simulation_transitions_subsets.remove(())
-
-        valid_transitions_set_exists = False
-        j = 0
-
-        while (not valid_transitions_set_exists) and j < len(simulation_transitions_subsets):
-            simulation_transitions_subset = list(simulation_transitions_subsets[j])
-
-            # si encontre un sub-conjunto de transiciones, cuya implicacion es satisfacible y ademas cae dentro de la aproximacion, entonces es valido
-            valid_transitions_set_exists = self._is_able_to_simulate_knowledge(knowledge, simulation_transitions_subset) and \
-                                           self.transitions_subset_fall_into_relation(simulation_transitions_subset, knowledge, relation)
-
-            j += 1
-
-        return valid_transitions_set_exists
-
-    def transitions_subset_fall_into_relation(self, simulation_transitions_subset, knowledge, relation):
-        fall_into_relation = False
-        k = 0
-
-        # me fijo si todas las transiciones del sub-conjunto caen dentro de la aproximacion que me pasaron por parametro
-        while not fall_into_relation and k < len(simulation_transitions_subset):
-            simulation_transition = simulation_transitions_subset[k]
-
-            related_element = (
-                self.target,
-                knowledge.union({self.assertion, simulation_transition.assertion}),
-                simulation_transition.target
-            )
-            fall_into_relation = related_element in relation
-
-            k += 1
-
-        return fall_into_relation
-
-    # Usamos z3-prover. Las assertions tienen que estar escritas con este framework.
-    def _is_able_to_simulate_knowledge(self, knowledge, simulation_transitions_subset):
-        simulation_assertions = set(map(lambda transition: transition.assertion, simulation_transitions_subset))
-
-        transition_knowledge = And(knowledge.union({self.assertion}))
-        simulation_transition_knowledge = And(knowledge.union({Or(simulation_assertions)}))
-
-        solver = Solver()
-
-        return solver.check(Implies(transition_knowledge, simulation_transition_knowledge)) == sat
