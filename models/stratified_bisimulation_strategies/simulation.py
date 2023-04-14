7be45173b9827f0ca4af6795beddbe9694044111
dsenarruzza <diegosenarruzza@gmail.com>
2023-04-14 01:15:40 -0300

    Some fixes on simulation

--- models/stratified_bisimulation_strategies/simulation.py
+++ models/stratified_bisimulation_strategies/simulation.py
@@ -15,7 +15,6 @@ class Simulation:
         self.simulator_knowledge = Knowledge(simulator_assertions_set)
 
         self.simulated_transition = None
-        self.simulator_transitions = set()
 
     def is_able_to_simulate(self):
         is_able = True
@@ -27,27 +26,23 @@ class Simulation:
         while is_able and i < len(simulated_transitions):
             self.simulated_transition = simulated_transitions[i]
 
-            cleaned_simulated_knowledge = self.simulated_knowledge.clean_by(self.simulated_transition.label)
-            cleaned_simulator_knowledge = self.simulator_knowledge.clean_by(self.simulated_transition.label)
-
-            self._set_simulator_transitions()
-
             # Necesito verificar si existe algun subconjunto de transiciones desde "simulator_state", que me sirva para simular la transicion de el "simulated_state"
             # Si existe, va a ser unico, ya que si existe mas de un subconjunto que hace esto, quiere decir que existen al menos dos transiciones desde "simulated_state"
             # tq. ambos caminos son validos para una traza valida. Esto nos daria un automata no-determinista, y estamos trabajando siempre con deterministas.
 
-            is_able = self._exists_a_valid_transition_subset_that_simulates(cleaned_simulated_knowledge, cleaned_simulator_knowledge)
+            is_able = self._exists_a_valid_transition_subset_that_simulates()
 
             i += 1
 
         return is_able
 
-    def _set_simulator_transitions(self):
-        self.simulator_transitions = self.simulator_state.get_transitions_with(self.simulated_transition.label)
+    def _exists_a_valid_transition_subset_that_simulates(self):
+        cleaned_simulated_knowledge = self.simulated_knowledge.clean_by(self.simulated_transition.label)
+        cleaned_simulator_knowledge = self.simulator_knowledge.clean_by(self.simulated_transition.label)
+        simulator_transitions = self.simulator_state.get_transitions_with(self.simulated_transition.label)
 
-    def _exists_a_valid_transition_subset_that_simulates(self, cleaned_simulated_knowledge, cleaned_simulator_knowledge):
         # No tomo el subconjunto vacio porque no seria valido. Si no hay mas conjuntos que el vacio, entonces nunca entra al loop y devuelve False
-        simulator_transitions_subsets = list(powerset(self.simulator_transitions))
+        simulator_transitions_subsets = list(powerset(simulator_transitions))
         simulator_transitions_subsets.remove(frozenset())
 
         valid_transitions_set_exists = False
@@ -55,14 +50,18 @@ class Simulation:
 
         while (not valid_transitions_set_exists) and j < len(simulator_transitions_subsets):
             simulator_transitions_subset = list(simulator_transitions_subsets[j])
-
-            # si encontre un sub-conjunto de transiciones, cuya implicacion es satisfacible y ademas cae dentro de la current_relation, entonces es valido
-            valid_transitions_set_exists = self._transitions_subset_fall_into_relation(simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge) and \
-                                           self._is_able_to_simulate_knowledge(simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge)
+            valid_transitions_set_exists = self._is_a_valid_transition_subset_to_simulate(simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge)
 
             j += 1
 
         return valid_transitions_set_exists
+
+    def _is_a_valid_transition_subset_to_simulate(self, simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge):
+        simulator_assertion = Assertion(Or({transition.assertion.expression for transition in simulator_transitions_subset}))
+
+        # si encontre un sub-conjunto de transiciones, cuya implicacion es satisfacible y ademas cae dentro de la current_relation, entonces es valido
+        return self._transitions_subset_fall_into_relation(simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge) and \
+               self._is_able_to_simulate_knowledge(simulator_assertion, cleaned_simulated_knowledge, cleaned_simulator_knowledge)
 
     def _transitions_subset_fall_into_relation(self, simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge):
         fall_into_current_relation = False
@@ -82,11 +81,9 @@ class Simulation:
 
         return fall_into_current_relation
 
-    def _is_able_to_simulate_knowledge(self, simulator_transitions_subset, cleaned_simulated_knowledge, cleaned_simulator_knowledge):
+    def _is_able_to_simulate_knowledge(self, simulator_assertion, cleaned_simulated_knowledge, cleaned_simulator_knowledge):
         simulated_knowledge = cleaned_simulated_knowledge.union(cleaned_simulator_knowledge).add(self.simulated_transition.assertion)
-        simulator_knowledge = cleaned_simulator_knowledge.add(
-            Assertion(Or({transition.assertion.expression for transition in simulator_transitions_subset}))
-        )
+        simulator_knowledge = cleaned_simulator_knowledge.add(simulator_assertion)
 
         implication = simulated_knowledge.build_implication_with(simulator_knowledge)
         solver = Solver()
